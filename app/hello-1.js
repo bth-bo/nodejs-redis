ddbe36d06de59234a292fc02f22ba5335b63ec44
banthita limwilai <bowbth.lwl@gmail.com>
2021-05-30 17:51:46 +0700

    modify js

--- app/hello-1.js
+++ app/hello-1.js
@@ -7,7 +7,8 @@ app.get('/hello1', function (req, res) {
   res.send("Hello-1")
 })
 
-app.listen(8000, () => console.log('Hello-1 listening on port 8000!'))
+// app.listen(8000, () => console.log('Hello-1 listening on port 8000!'))
+app.listen(8000, () => console.log('Hello-1 modify listening on port 8000!'))
 
 function writeSession() {
   var dt = new Date();
