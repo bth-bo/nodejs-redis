06f47d1d2c268212b212b6525b4955aef3cad5ca
banthita limwilai <bowbth.lwl@gmail.com>
2021-05-31 23:29:40 +0700

    edit test.js

--- test/test.js
+++ test/test.js
@@ -5,6 +5,6 @@ const app = require("../app/hello-2")
 
 describe("GET /", () => {
     it("respond with Hello-2", (done) => {
-        request(app).get("/").expect("Hello-2", done)
+        request(app).get("/hello2").expect("Hello-2", done)
     })
 });
